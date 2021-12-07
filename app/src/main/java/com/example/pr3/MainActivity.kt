e7093ea64ab344a80f20a4cd09757199e5557e6b
Akshat1507 <patelakshat8801@gmail.com>
2021-12-07 12:25:14 +0530

    first commit

---
+++ app/src/main/java/com/example/pr3/MainActivity.kt
@@ -0,0 +1,15 @@
+    package com.example.pr3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val actionBar=supportActionBar
+        actionBar!!.title="practical_2"
+
+    }
+}
